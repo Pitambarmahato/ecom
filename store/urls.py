6260b4c88d4fd32e19b822588e4aa2907efd07b3
Pitambarmahato <pitambarmahato890@gmail.com>
2021-06-11 00:35:45 +0545

    Frontend part completed

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout')
 ]
--- store/views.py
+++ store/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, 'store/base.html')
+def store(request):
+    return render(request, 'store/store.html')
+
+def cart(request):
+    return render(request, 'store/cart.html')
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
